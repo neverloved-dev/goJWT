e134a7bd16db8c489e5ed3d4a0404af5fb07aafb
Andjic Djordje <neverloved.dev@gmail.com>
2024-09-21 22:37:04 +0200

    Get the server up and running

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handleReturnPong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func main() {
 	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/ping", handleReturnPong)
+	r.Run(":9000")
 }
